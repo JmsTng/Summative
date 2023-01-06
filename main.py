bbfe1d0f255a5bde9318bf6395e4085283d2ac10
James Tung <95390086+JmsTng@users.noreply.github.com>
2023-01-06 16:59:49 -0500

    port

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import os
 import sys
 
 import pygame
-from player import Player
+from objects import Player, Solid
 
 
 ### CONSTANTS ###
 pygame.init()
-pygame.key.set_repeat(10)
 
 # COLOURS
 black = (0, 0, 0)
@@ -18,14 +17,39 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode()
 size = width, height = screen.get_size()
 center = (width//2, height//2)
+print(size)
 
 # PLAYERS
-P1 = Player(os.path.join("assets", "character1.png"), ('w', 'a', 's', 'd'), y=center[1]-32)
-P2 = Player(os.path.join("assets", "character2.png"), ('u', 'l', 'd', 'r'), y=center[1]+32)
+P1 = Player(
+    screen,
+    os.path.join("assets", "Character1.png"),
+    (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d),
+    speed=5,
+    x=center[0]-32,
+    y=center[1]
+)
+P2 = Player(
+    screen,
+    os.path.join("assets", "Character2.png"),
+    (pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT),
+    speed=5,
+    x=center[0]+32,
+    y=center[1]
+)
+log = Solid(
+    screen,
+    os.path.join("assets", "log_sprite.png"),
+    x=center[0],
+    y=center[1]
+)
+
+# INTERACTION
+pygame.key.set_repeat(10)
 
 
 ### EVENT LOOP ###
 while True:
+    screen.fill(black)
     pygame.event.pump()
 
     for event in pygame.event.get():
@@ -34,16 +58,17 @@ while True:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             pressed = pygame.key.get_pressed()
-            keys = ['']*4
-            keys.append('w' if pressed[pygame.K_w] else '')
-            keys.append('a' if pressed[pygame.K_a] else '')
-            keys.append('s' if pressed[pygame.K_s] else '')
-            keys.append('d' if pressed[pygame.K_d] else '')
-            P1.move(screen, keys)
-            keys = ['']*4
-            keys.append('u' if pressed[pygame.K_UP] else '')
-            keys.append('l' if pressed[pygame.K_LEFT] else '')
-            keys.append('d' if pressed[pygame.K_DOWN] else '')
-            keys.append('r' if pressed[pygame.K_RIGHT] else '')
-            P2.move(screen, keys)
-            
+            keys = ['']*8
+            for key in P1.keys+P2.keys:
+                if pressed[key]:
+                    keys.append(key)
+            P1.move(keys)
+            P2.move(keys)
+            log.collides((P1, P2))
+
+    log.render()
+    P1.render(screen)
+    P2.render(screen)
+
+
+    pygame.display.flip()
