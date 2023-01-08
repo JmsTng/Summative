c1b1a314a193f8b82e42f9c85dd72825bf426b55
JmsTng <james.ftung@gmail.com>
2023-01-08 16:30:26 -0500

    refactored

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 import pygame
-from objects import Player
+from objects import Player, Object
 
 
 ### CONSTANTS ###
@@ -17,14 +17,20 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode()
 size = width, height = screen.get_size()
 center = (width//2, height//2)
-print(size)
 
-# PLAYERS
+# OBJECTS
+walls = [
+    Object(canvas=screen, x=-1, y=-1, w=2, h=height), # left
+    Object(canvas=screen, x=width-1, y=-1, w=2, h=height+2), # right
+    Object(canvas=screen, x=-1, y=-1, w=width+2, h=2), # top
+    Object(canvas=screen, x=-1, y=height-1, w=width+2, h=2)]
+
+speed = width//1000
 P1 = Player(
     canvas=screen,
     path=os.path.join("assets", "Character1.png"),
     keys=(pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d),
-    speed=5,
+    speed=speed,
     x=center[0]-32,
     y=center[1],
     img_scale=2
@@ -33,14 +39,16 @@ P2 = Player(
     canvas=screen,
     path=os.path.join("assets", "Character2.png"),
     keys=(pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT),
-    speed=5,
+    speed=speed,
     x=center[0]+32,
     y=center[1],
     img_scale=2
 )
 
+box = Object(canvas=screen, path=r'assets\Stone1.png', x=400, y=123, img_scale=3)
+
 # INTERACTION
-pygame.key.set_repeat(10)
+pygame.key.set_repeat(5*speed)
 
 
 ### EVENT LOOP ###
@@ -49,19 +57,27 @@ while True:
     pygame.event.pump()
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            pressed = pygame.key.get_pressed()
-            keys = ['']*8
-            for key in P1.keys+P2.keys:
-                if pressed[key]:
-                    keys.append(key)
-            P1.move((P2,), keys)
-            P2.move((P1,), keys)
+            match event.key:
+                case pygame.K_r:
+                    P1.rect.x, P1.rect.y = center[0]-32, center[1]
+                    P2.rect.x, P2.rect.y = center[0]+32, center[1]
+        
+    if any(pressed := pygame.key.get_pressed()):
+        keys = ['']*8
+        for key in P1.keys+P2.keys:
+            if pressed[key]:
+                keys.append(key)
+        P1.move((P2,*walls, box), keys)
+        P2.move((P1,*walls), keys)
 
     P1.render(screen)
     P2.render(screen)
+    for wall in walls:
+        wall.render(color=white, solid=True)
+    box.render()
 
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -2,20 +2,23 @@ import pygame
 
 class Object:
     '''Base class for all entities.'''
-    def __init__(self, canvas:pygame.Surface, path:str, x:int=0, y:int=0, img_scale:float=1):
-        img = pygame.image.load(path)
-        w = img.get_width()
-        h = img.get_height()
+    def __init__(self, canvas:pygame.Surface, path:str=None, color:str=None, x:int=0, y:int=0, w:int=0, h:int=0, img_scale:float=1):
+        if path: # from path
+            img = pygame.image.load(path)
+            w = img.get_width()
+            h = img.get_height()
+            self.img = pygame.transform.scale(img, (w*img_scale, h*img_scale))
+        else: # 
+            self.color = color
         self.canvas = canvas
-        self.img = pygame.transform.scale(img, (w*img_scale, h*img_scale))
-        self.rect = pygame.Rect(x, y, w, h)
+        self.rect = pygame.Rect(x, y, w*img_scale, h*img_scale)
         
     def near(self, entities):
         entities = list(entities)
         
         for e in entities:
-            if self.rect.right not in range(e.rect.left-64, e.rect.left+128) \
-               or self.rect.bottom not in range(e.rect.top-64, e.rect.top+128):
+            if self.rect.right not in range(e.rect.left-64, e.rect.left+160) or \
+               self.rect.bottom not in range(e.rect.top-64, e.rect.top+160):
                 entities.remove(e)
                 continue
         
@@ -26,37 +29,41 @@ class Object:
         free = ['l', 'r', 'u', 'd']
         for e in entities:
             e = e.rect
-            if self.rect.clipline(e.x+e.w, e.y, e.x+e.w, e.y+e.h):
+            if self.rect.clipline(e.right, e.top, e.right, e.bottom):
                 free.remove('l')
-            if self.rect.clipline(e.x, e.y, e.x, e.y+e.h):
+            if self.rect.clipline(e.left, e.top, e.left, e.bottom):
                 free.remove('r')
-            if self.rect.clipline(e.x, e.y+e.h, e.x+e.w, e.y+e.h):
+            if self.rect.clipline(e.left, e.bottom, e.right, e.bottom):
                 free.remove('u')
-            if self.rect.clipline(e.x, e.y, e.x+e.w, e.y):
+            if self.rect.clipline(e.left, e.top, e.right, e.top):
                 free.remove('d')
         
         return free
         
-    def render(self, canvas=None):
+    def render(self, canvas=None, color=None, solid=False):
         canvas = self.canvas if not canvas else canvas
-        canvas.blit(self.img, self.rect)
+        if solid:
+            pygame.draw.rect(canvas, color, self.rect)
+        else:
+            canvas.blit(self.img, self.rect)
         
 class Player(Object):
+    '''Class to represent playble characters.'''
     def __init__(self, *, canvas:pygame.Surface, path:str, keys:tuple, speed:float, x:int=0, y:int=0, img_scale:float=1):
-        super().__init__(canvas, path, x, y, img_scale)
+        super().__init__(canvas=canvas, path=path, x=x, y=y, img_scale=img_scale)
         self.keys = keys
         self.speed = speed
 
     def move(self, entities, keys):
         free = self.collides(entities)
-        vector = (0, 0)
-        if self.keys[0] in keys and free.index('u'):
-            vector[1] -= 5
-        if self.keys[1] in keys and free.index('l'):
-            vector[0] -= 5
-        if self.keys[2] in keys and free.index('d'):
-            vector[1] += 5
-        if self.keys[3] in keys and free.index('r'):
-            vector[0] += 5
+        vector = [0, 0]
+        if self.keys[0] in keys and free.count('u'):
+            vector[1] -= self.speed
+        if self.keys[1] in keys and free.count('l'):
+            vector[0] -= self.speed
+        if self.keys[2] in keys and free.count('d'):
+            vector[1] += self.speed
+        if self.keys[3] in keys and free.count('r'):
+            vector[0] += self.speed
             
         self.rect.move_ip(vector)
